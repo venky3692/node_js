23076692e1a40275932bacf3592f368a4c369323
venky3692 <venkatesh.iyer@tarento.com>
2023-02-10 10:09:19 +0530

    d-1

--- tutorial.js
+++ tutorial.js
@@ -1,3 +1,16 @@
-const app = require("./app")
+const fs = require('fs')
 
+const app = require("./app")
 console.log(app.c("Venkatesh")) 
+
+const arr = [1,2,3,4,5]
+
+let result = arr.filter((item)=>{
+    return item>4
+})
+console.log(result)
+
+fs.writeFileSync("Hello.txt", "I am venkatesh and I am learning Node JS")
+
+console.log("This id dirname ==>", __dirname)
+console.log("This is file name ==>", __filename)
